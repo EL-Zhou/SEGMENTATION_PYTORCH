c474ec8acaeea6675df5b8567507fe5856ee805b
yiren-jian <yirenjian0621@gmail.com>
2021-01-07 02:53:34 -0500

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='SEGMENTATION_PYTORCH',
+      install_requires=['pyyaml', 'tensorboardX',
+                        'easydict', 'matplotlib',
+                        'scipy', 'scikit-image',
+                        'future', 'setuptools',
+                        'tqdm', 'cffi'],
+      packages=find_packages())
